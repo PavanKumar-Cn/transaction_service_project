e31e0c0984730acc15a034f9e714f31da94704f1
Umamahesh8074 <152247751+Umamahesh8074@users.noreply.github.com>
2024-02-02 09:39:17 +0530

    gitigonre added
    done

--- JAVA_Project/transaction_service/src/main/java/com/transaction_service/repository/UserRepository.java
+++ JAVA_Project/transaction_service/src/main/java/com/transaction_service/repository/UserRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.transaction_service.entity.User;
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> b4273822b0d416ac2e51009399a0de7a930aa3b2
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
