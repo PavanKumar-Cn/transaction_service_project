547813e7612615b914a2c93fafcd51eb05f82c97
Umamahesh8074 <152247751+Umamahesh8074@users.noreply.github.com>
2024-02-02 10:05:13 +0530

    u  userservice added
    u

---
+++ JAVA_Project/transaction_service/src/main/java/com/transaction_service/service/UserService.java
@@ -0,0 +1,13 @@
+package com.transaction_service.service;
+
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import com.transaction_service.entity.User;
+
+public interface UserService {
+	User saveUser(User user);
+
+	User updateUser(User user, Integer admin) throws NotFoundException;
+
+	User findUserById(Integer userId) throws NotFoundException;
+}
